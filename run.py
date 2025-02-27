8846af0bf2bf0a9262074c85a928adbcf042567b
Tomas <tomas@git>
2025-02-27 19:50:46 +0100

    Reformatsdfsdfsdf

--- run.py
+++ run.py
@@ -58,8 +58,8 @@ def track_memory():
 
 
 if __name__ == "__main__":
-    debug = os.getenv("DEBUG", "False").lower() == "true"
-    port = int(os.getenv("PORT", 8080))
+    debug = os.getenv("DEBUG", "False")
+    port = int(os.getenv("PORT", 5000))
     app.run(
         host="0.0.0.0",
         port=port,
